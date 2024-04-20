25b60c3e69ea80705aed03ac1658175b3da34de4
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2024-04-19 17:50:45 -0700

    route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,20 +12,21 @@ app = Flask("__name__")
 
     def hello():
     """Return a given string"""
-        return ("Hello HBNB!")
+    return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
-    def hbnb():
+
+def hbnb():
     """Returns a given string"""
-        return ("HBNB")
+    return ("HBNB")
 
 
 @app.route("/c/<text>", strict_slashes=False)
-    def cText(text):
+
+def cText(text):
     """display C followed by the value of the text variable"""
-        return "C {}".format(text.replace("_", " "))
+    return "C {}".format(text.replace("_", " "))
 
-
-    if __name__ == "__main__":
+if __name__ == "__main__":
         app.run(host="0.0.0.0", port=5000, debug=None)
