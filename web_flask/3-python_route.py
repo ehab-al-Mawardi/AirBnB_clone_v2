e9ad16177610e2f38f52d4cd05e1f0acdd552340
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2024-04-19 18:25:31 -0700

    Py route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,72 +1,35 @@
 #!/usr/bin/python3
-import os.path
-from datetime import datetime
-from fabric.api import env
-from fabric.api import local
-from fabric.api import put
-from fabric.api import run
+"""
+start Flask application
+"""
 
-env.hosts = ['54.160.97.148', '52.87.235.176']
+from flask import Flask
+app = Flask(__name__)
 
-def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                        dt.month,
-                                                        dt.day,
-                                                        dt.hour,
-                                                        dt.minute,
-                                                        dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
 
-def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+        return 'Hello HBNB!'
 
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-            format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-            format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-            format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-            format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(name)).failed is True:
-        return False
-    return True
+@app.route('/hbnb', strict_slashes=False)
 
-def deploy():
-    """Create and distribute an archive to a web server."""
-    file = do_pack()
-    if file is None:
-        return False
-    return do_deploy(file)
+def hbnb():
+    """returns HBNB"""
+        return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+        return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+        return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
