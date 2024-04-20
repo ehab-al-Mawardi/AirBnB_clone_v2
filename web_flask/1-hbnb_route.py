579406e08c170c4375f4a812797d58ee60b84a16
ehab-al-Mawardi <ihabalmawardi@gmail.com>
2024-04-19 17:29:42 -0700

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,9 +3,9 @@
     Your web application must be listening on 0.0.0.0, port 5000.
     """
 
-from flask import Flask
+    from flask import Flask
 
-app = Flask(__name__i
+    app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 
@@ -13,11 +13,10 @@ def hello():
     """Return a given string."""
         return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
-
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return a given string."""
         return "HBNB"
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
